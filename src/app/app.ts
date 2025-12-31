b6c16bf46b5050a91a33402082743530ee16b6ad
aymanmossa <aymanmossa35@gmail.com>
2025-12-31 22:48:21 +0200

    Revert PR #1 (feature/Services) and restore previous state

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar';
 import { FooterComponent } from './shared/components/footer/footer';
-import { Services } from "./services/services";
-import { Ads } from './ads/ads';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, Services,Ads],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
