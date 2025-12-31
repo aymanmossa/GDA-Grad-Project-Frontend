47b1fafd2e27aa052f108e697c5568e20aeb0e4a
AbdallahNassar99 <abdallahnassar2030@gmail.com>
2025-12-31 20:21:11 +0200

    add new component services

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar';
 import { FooterComponent } from './shared/components/footer/footer';
+import { Services } from "./services/services";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, Services],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
