57402198a63308df83c1f5e1be266476a55a41b3
AbdallahNassar99 <abdallahnassar2030@gmail.com>
2025-12-31 22:00:05 +0200

    add  Carousel

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar';
 import { FooterComponent } from './shared/components/footer/footer';
 import { Services } from "./services/services";
+import { Ads } from './ads/ads';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, Services],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, Services,Ads],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
